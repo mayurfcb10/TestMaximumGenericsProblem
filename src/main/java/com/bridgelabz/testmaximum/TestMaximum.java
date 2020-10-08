c983ec72141920eb261a1f661106de8a1127497c
Mayur <mayurzope555@gmail.com>
2020-10-08 15:01:38 +0530

    [Mayur]Added: Welcome to the Test Maximum Generics Program

---
+++ src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.testmaximum;
+
+public class TestMaximum {
+	/* Welcome message */
+	public static void printWelcomeMessage() {
+		System.out.println("Welcome to the Test Maximum Program");
+	}
+}
---
+++ src/test/java/com/bridgelabz/testmaximum/TestMaximumJUnitTest.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.testmaximum;
+
+import org.junit.Test;
+
+public class TestMaximumJUnitTest {
+	/* Welcome message test method */
+	@Test
+	public void shouldPrintWelcomeMessage() {
+		TestMaximum.printWelcomeMessage();
+	}
+}
