c0aad2ef39b6abc71bcd572676be89bb32b7d97f
Mayur <mayurzope555@gmail.com>
2020-10-08 15:05:56 +0530

    [Mayur]Added: Maximum of 3 Integers with Junit Test Case

--- src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
+++ src/main/java/com/bridgelabz/testmaximum/TestMaximum.java
@@ -5,4 +5,14 @@ public class TestMaximum {
 	public static void printWelcomeMessage() {
 		System.out.println("Welcome to the Test Maximum Program");
 	}
+
+	/* maximum of 3 Integer values at test position */
+	public static Integer maximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+		Integer maximumValue = firstValue;
+		if (secondValue.compareTo(maximumValue) > 0)
+			maximumValue = secondValue;
+		if (thirdValue.compareTo(maximumValue) > 0)
+			maximumValue = thirdValue;
+		return maximumValue;
+	}
 }
--- src/test/java/com/bridgelabz/testmaximum/TestMaximumJUnitTest.java
+++ src/test/java/com/bridgelabz/testmaximum/TestMaximumJUnitTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.testmaximum;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMaximumJUnitTest {
@@ -7,5 +8,26 @@ public class TestMaximumJUnitTest {
 	@Test
 	public void shouldPrintWelcomeMessage() {
 		TestMaximum.printWelcomeMessage();
+	}
+
+	/* Maximum Integer element at the position 1 */
+	@Test
+	public void maximumIntegerAtPosition1() {
+		Integer maximumInteger = TestMaximum.maximumInteger(12, 4, 11);
+		Assert.assertSame(12, maximumInteger);
+	}
+
+	/* Maximum Integer element at the position 2 */
+	@Test
+	public void maximumIntegerAtPosition2() {
+		Integer maximumInteger = TestMaximum.maximumInteger(4, 12, 11);
+		Assert.assertSame(12, maximumInteger);
+	}
+
+	/* Maximum Integer element at the position 3 */
+	@Test
+	public void maximumIntegerAtPosition3() {
+		Integer maximumInteger = TestMaximum.maximumInteger(4, 11, 12);
+		Assert.assertSame(12, maximumInteger);
 	}
 }
